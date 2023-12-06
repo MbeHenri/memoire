3469eb674dfe1891b851ba955002a36a7b81cbd0
MbeHenri <henri.mbe@facsciences-uy1.cm>
2023-12-06 13:06:02 +0100

    mise a jour mcles

--- utils/mcles/scratch/updateS.py
+++ utils/mcles/scratch/updateS.py
@@ -1,7 +1,8 @@
 from numpy import dot, eye, ones, zeros, maximum
 from numpy.linalg import norm
-
 from qpsolvers import solve_qp
+from scipy.sparse import csc_matrix
+from warnings import catch_warnings, simplefilter
 
 # fonction de mise à jour de S
 def UpdateS(H, P, alpha=0.001, beta=0.5, gamma=0.004):
@@ -11,27 +12,32 @@ def UpdateS(H, P, alpha=0.001, beta=0.5, gamma=0.004):
     K = dot(H.T, H)
     Q = K + (beta/alpha) * eye(K.shape[0], dtype=float)
     Q = (Q + Q.T)/2
-
+    
     # pour les contraintes linéaire
 
     A = ones((N, N), dtype=float)
+    A = csc_matrix(A)
+    
     ones_ = ones(N, dtype=float)  # uh , b
     zeros_ = zeros(N, dtype=float)  # lh
 
     S_ = zeros((N, N))
-    for i in range(N):
-
-        # calcul de bi
-        for j in range(N):
-            bi[j] = norm(P[i] - P[j])**2
-        
-        bi = maximum(bi.real, 0)
-        
-        # calcul de r
-        r = -2*K[i, :] + (gamma / (2*alpha))*bi
-
-        # calcul de S:i
-        Si = solve_qp(Q, r, None, None, A, ones_, zeros_, ones_, solver="osqp")
-        if Si is not None:
-            S_[:, i] = Si
+    
+    with catch_warnings():
+        simplefilter("ignore", UserWarning)
+        for i in range(N):
+    
+            # calcul de bi
+            for j in range(N):
+                bi[j] = norm(P[i] - P[j])**2
+            
+            bi = maximum(bi.real, 0)
+            
+            # calcul de r
+            r = -2*K[i, :] + (gamma / (2*alpha))*bi
+    
+            # calcul de S:i
+            Si = solve_qp(Q, r, None, None, A, ones_, zeros_, ones_, solver="osqp")
+            if Si is not None:
+                S_[:, i] = Si
     return S_
